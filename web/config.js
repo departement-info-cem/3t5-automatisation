866caf46ca10c6667f4078b15d20e17171bba27c
Vincent Carrier <51422326+vcarrier@users.noreply.github.com>
2024-06-03 12:54:09 -0400

    Configuration initiale

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
-    nom: "Code - Nom du cours",
-    description: "Site de référence du cours de x",
-    nomUrl: "depinfo-gabarit"
+    nom: "420-3T5-EM Automatisation de tâches",
+    description: "Site de référence du cours d'automatisation de tâches",
+    nomUrl: "3t5-automatisation"
 }
 
 module.exports = config;
